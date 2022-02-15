eb8ffa3f779c8435f59295c7c2966c9aaf1867ea
Harvey Francois <har.francois@gmail.com>
2022-02-14 19:27:19 -0800

    created the bare bone

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+       <p>&copy: Harvey Francois</p>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <HornedBeast title='somtheing'
+        imageUrl="#"
+        description='something'
+        />
+        <HornedBeast title='somtheing'
+        imageUrl="#"
+        description='something'
+        />
+        <HornedBeast title='somtheing'
+        imageUrl="#"
+        description='something'
+        />
+
+      </main>
+    );
+  }
+}
+
+export default Main;
