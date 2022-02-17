ea8b87152bdac70a99f14d6189c15ccf1d2d1ed5
Harvey Francois <har.francois@gmail.com>
2022-02-16 23:41:43 -0800

    not done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,56 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import data from './data.json';
+import { Modal } from 'react-bootstrap';
+
+
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false,
+      name: '',
+      image: '',
+      about: 'about'
+    }
+  }
+  
+  handelCloseModal = () => {
+    this.setState({
+      showModel: false,      
+    })
+  }
+  handelShowModal = (beast) => {
+    this.setState({
+      showModal: true,
+      name: beast
+    })
+  }
+  getImages = (img) => {
+    this.setState({
+      image: img
+    })
+  }
   render() {
     return (
     <>
       <Header />
-      <Main data={data}/>
+      <Modal show={this.state.showModal} onHide={this.state.handelCloseModal}>
+        <Modal.Header closeButton>
+        <Modal.Title>{this.state.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img alt={this.state.name}></img>
+        </Modal.Body> 
+      </Modal>
+      <Main 
+        data={data}
+        handelShowModal={this.handelShowModal}
+        getImage={this.getImage}
+
+
+      />
       <Footer/>
     </>
     );
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './hornedBeast.css';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -14,23 +16,27 @@ class HornedBeast extends React.Component {
       likes: this.state.likes + 1
     })
   }
-
+  handelClick = () => {
+    this.handelLikes()
+    this.props.handelShowModal(this.props.title)
+  }
+  getImage = () => {
+    this.props.getImages(this.props.imageUrl)
+  }
   render() {
 
     return (
       <>
-      <p id='heart'>💜 {this.state.likes}</p>
+        <p id='heart'>💜 {this.state.likes}</p>
         <Card border="dark" style={{ width: '18rem'}}>
           <Card.Body >
             <Card.Title><h2>{this.props.title}</h2></Card.Title>
-            <Card.Img onClick={this.handelLikes} src={this.props.imageUrl}
+            <Card.Img onClick={this.handelClick} src={this.props.imageUrl}
             ></Card.Img>
             <Card.Text>{this.props.description}</Card.Text>
           </Card.Body>
         </Card>
-        <br />
       </>
-
     );
   }
 }
